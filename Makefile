26bca62b791320543a2395ca4a52858371048ad6
BaldyAsh <antongrigorjev2010@gmail.com>
2019-08-05 17:53:48 +0300

    init

---
+++ Makefile
@@ -0,0 +1,11 @@
+CXX_FLAGS := -Wall -Wextra -std=c++17 -O3 -fPIC
+
+SRC		:= .
+INCLUDE	:= include 
+LIBS 	:= -Bstatic -lstdc++
+
+all: library
+
+library: $(SRC)/*.cpp
+	$(CXX) $(CXX_FLAGS) -isystem$(INCLUDE) $^ $(LIBS) -shared -o libeip1962.so
+
---
+++ eip1962.go
@@ -0,0 +1,55 @@
+package eip1962
+
+/*
+#cgo CXXFLAGS: -std=c++17
+#cgo CXXFLAGS: -I./include
+#include "wrapper.h"
+*/
+import "C"
+
+import (
+	"errors"
+	"math/big"
+	"unsafe"
+)
+
+const maxOutputLen = 256 * 3 * 2
+
+var (
+	ErrInvalidMsgLen = errors.New("invalid data length, need >= bytes")
+	ErrCallFailed    = errors.New("library call returned an error")
+)
+
+// Call calls the C++ implementation of the EIP
+func Call(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, ErrInvalidMsgLen
+	}
+	ilen := len(data)
+	outputBytes := make([]byte, maxOutputLen)
+	olen := uint32(0)
+	errStringBytes := make([]byte, maxOutputLen)
+	errStringLen := uint32(0)
+
+	var (
+		inputdata  = (*C.char)(unsafe.Pointer(&data[0]))
+		inputlen   = (C.uint32_t)(ilen)
+		outputdata = (*C.char)(unsafe.Pointer(&outputBytes[0]))
+		outputlen  = (*C.uint32_t)(unsafe.Pointer(&olen))
+		errdata    = (*C.char)(unsafe.Pointer(&errStringBytes[0]))
+		errlen     = (*C.uint32_t)(unsafe.Pointer(&errStringLen))
+	)
+
+	result := C.run(inputdata, inputlen, outputdata, outputlen, errdata, errlen)
+	if result == 0 {
+		// parse error string
+		return nil, ErrCallFailed
+	}
+
+	return outputBytes[:olen], nil
+}
+
+// EstimateGas calls C++ implementation for a gas estimte
+func EstimateGas(data []byte) *big.Int {
+	return big.NewInt(1000000)
+}
